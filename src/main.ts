90998b775e43acb328e1fa9395c4866abdff37fe
jsconan <jsconan@gmail.com>
2025-08-22 23:21:35 +0200

    feat: add a PCI registry implementation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 export { AMDLoader } from 'lib/amd-loader.ts';
+export { PCIRegistry } from 'lib/pci-registry.ts';
 
 export type { PCI } from 'lib/pci.d.ts';
 export type { SystemJS } from 'lib/systemjs.d.ts';

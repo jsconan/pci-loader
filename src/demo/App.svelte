e21ed4b71810afb4a9d736293f28969946b70339
jsconan <jsconan@gmail.com>
2025-08-23 00:41:19 +0200

    feat: add a playground to demonstrate how to use the library

--- src/demo/App.svelte
+++ src/demo/App.svelte
@@ -1,11 +1,67 @@
 <script lang="ts">
+    import Playground from 'demo/components/Playground.svelte';
+
+    const samples: Record<string, components.Sample> = {
+        pci: {
+            type: 'pci',
+            url: '/samples/pci.js',
+            description: 'PCI example.'
+        },
+        pciLoadFailure: {
+            type: 'pci',
+            url: '/samples/pci-load-failure.js',
+            description: 'PCI failure at loading.'
+        },
+        pciRenderFailure: {
+            type: 'pci',
+            url: '/samples/pci-render-failure.js',
+            description: 'PCI failure at rendering.'
+        },
+        bundle: {
+            type: 'amd',
+            url: '/samples/bundle.js',
+            description: 'Bundle example.'
+        },
+        split: {
+            type: 'amd',
+            url: '/samples/internal.js',
+            description: 'Split example.',
+            dependencies: [
+                {
+                    name: 'external',
+                    url: '/samples/external.js'
+                }
+            ]
+        },
+        resource: {
+            type: 'amd',
+            url: '/samples/resource.js',
+            description: 'Resource example.'
+        },
+        exports: {
+            type: 'amd',
+            url: '/samples/exports.js',
+            description: 'Exports example.'
+        },
+        invalid: {
+            type: 'amd',
+            url: '/samples/invalid.js',
+            description: 'Invalid example.'
+        },
+        error: {
+            type: 'amd',
+            url: '/samples/error.js',
+            description: 'Error example.'
+        }
+    };
 </script>
 
 <main>
     <div class="title">
         <h1>PCI Loader Demo</h1>
-        <p>Welcome to the PCI Loader demo! This application showcases the use of the library.</p>
+        <p>Welcome to the PCI Loader demo! This application demonstrates how to use the library.</p>
     </div>
+    <Playground {samples} />
 </main>
 
 <style>
--- src/demo/types.d.ts
+++ src/demo/types.d.ts
@@ -1,4 +1,16 @@
 declare namespace components {
+    export type Sample = {
+        type: 'pci' | 'amd';
+        url: string;
+        description: string;
+        dependencies?: Dependency[];
+    };
+
+    export type Dependency = {
+        name: string;
+        url: string;
+    };
+
     export type DropdownItem = {
         value: string;
         label: string;

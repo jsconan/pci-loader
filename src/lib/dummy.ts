60f2e9941482620bdb84ed3783b251277846cf40
jsconan <jsconan@gmail.com>
2025-08-22 21:46:33 +0200

    chore: remove the dummy placeholder from the lib

--- src/lib/dummy.ts
+++
@@ -1,3 +0,0 @@
-export function dummy(): string {
-    return 'dummy';
-}
--- tests/main.test.ts
+++
@@ -1,8 +0,0 @@
-import { dummy } from 'lib/dummy.ts';
-import { describe, expect, it } from 'vitest';
-
-describe('main', () => {
-    it('dummy test', async () => {
-        expect(dummy()).toBe('dummy');
-    });
-});

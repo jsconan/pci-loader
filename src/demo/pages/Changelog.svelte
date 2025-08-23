f5dd1b440b04108544ee37ec167c11acd50aa439
jsconan <jsconan@gmail.com>
2025-08-23 23:20:07 +0200

    feat: add a page showing the project's changelog

---
+++ src/demo/pages/Changelog.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import SvelteMarkdown from '@humanspeak/svelte-markdown';
+
+    import source from 'root/CHANGELOG.md';
+</script>
+
+<SvelteMarkdown {source} />
+
+<style>
+</style>
--- src/demo/router.ts
+++ src/demo/router.ts
@@ -1,3 +1,4 @@
+import Changelog from 'demo/pages/Changelog.svelte';
 import Demo from 'demo/pages/Demo.svelte';
 import License from 'demo/pages/License.svelte';
 import NotFound from 'demo/pages/NotFound.svelte';
@@ -7,6 +8,7 @@ import { createRouter } from 'sv-router';
 export const { p, navigate, isActive, route } = createRouter({
     '/': Overview,
     '/demo': Demo,
+    '/changelog.md': Changelog,
     '/license': License,
     '*': NotFound
 });

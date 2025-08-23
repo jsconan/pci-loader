f86a02aa3dcd80878e4ab2932aaed401232f5dca
jsconan <jsconan@gmail.com>
2025-08-23 23:18:24 +0200

    feat: add an overview page showing the project's readme

---
+++ src/demo/pages/Overview.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import Markdown from 'demo/components/Markdown.svelte';
+    import source from 'root/README.md';
+</script>
+
+<Markdown {source} />
--- src/demo/router.ts
+++ src/demo/router.ts
@@ -1,8 +1,10 @@
 import Demo from 'demo/pages/Demo.svelte';
 import NotFound from 'demo/pages/NotFound.svelte';
+import Overview from 'demo/pages/Overview.svelte';
 import { createRouter } from 'sv-router';
 
 export const { p, navigate, isActive, route } = createRouter({
-    '/': Demo,
+    '/': Overview,
+    '/demo': Demo,
     '*': NotFound
 });

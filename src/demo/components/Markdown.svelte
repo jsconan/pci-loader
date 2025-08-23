2c2036fff1c5026fe0e3fedd840a806a20725edc
jsconan <jsconan@gmail.com>
2025-08-23 22:54:41 +0200

    feat: add a component for rendering markdown content

---
+++ src/demo/components/Markdown.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import SvelteMarkdown, { defaultRenderers } from '@humanspeak/svelte-markdown';
+    import MarkdownCode from './MarkdownCode.svelte';
+
+    interface Props {
+        source: string;
+    }
+
+    let { source }: Props = $props();
+
+    const renderers = {
+        ...defaultRenderers,
+        code: MarkdownCode
+    };
+</script>
+
+<div class="markdown">
+    <SvelteMarkdown {source} {renderers} />
+</div>
+
+<style>
+    div {
+        max-width: 1280px;
+        width: 100%;
+        margin: 0 auto;
+        padding: 1em;
+        border: 1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        background: var(--background);
+    }
+</style>
---
+++ src/demo/components/MarkdownCode.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Code from 'demo/components/Code.svelte';
+    import CopyToClipboard from 'demo/components/CopyToClipboard.svelte';
+
+    interface Props {
+        lang: string;
+        text: string;
+    }
+
+    let { lang, text }: Props = $props();
+
+    const supportedLanguages = ['js', 'ts', 'javascript', 'typescript'];
+</script>
+
+{#if supportedLanguages.includes(lang)}
+    <Code code={text} />
+{:else}
+    <CopyToClipboard content={text}>
+        <pre class={lang}><code>{text}</code></pre>
+    </CopyToClipboard>
+{/if}
+
+<style>
+    pre {
+        background: var(--code-background);
+        color: var(--code-text);
+        padding: 1em;
+        border-radius: 0.5em;
+        overflow: auto;
+    }
+</style>

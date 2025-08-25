e3ff455070a4180c74ea0db04fb0698808bca61d
jsconan <jsconan@gmail.com>
2025-08-25 16:00:40 +0200

    feat: style inline code

---
+++ src/demo/components/MarkdownCodespan.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    interface Props {
+        raw: string;
+    }
+
+    const { raw }: Props = $props();
+</script>
+
+<code>{raw.replace(/`/g, '')}</code>
+
+<style>
+    code {
+        padding: 0.2em 0.4em;
+        font-weight: 600;
+        color: var(--text-color);
+        background-color: var(--background-highlight);
+        border-radius: var(--code-border-radius);
+    }
+</style>

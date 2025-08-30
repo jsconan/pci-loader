59d013c39081fb0b8d0b9e72f2ac27f59515be79
jsconan <jsconan@gmail.com>
2025-08-30 17:01:44 +0200

    feat: add a development implementation of the scoped PCI loader

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 export { AMDLoader } from 'lib/amd-loader.ts';
-export { PCILoader } from 'lib/pci-loader.ts';
+export { PCILoader, PCILoaderDev } from 'lib/pci-loader.ts';
 export { PCIRegistry } from 'lib/pci-registry.ts';
 export { TimeoutError } from 'lib/timeout.ts';
 
